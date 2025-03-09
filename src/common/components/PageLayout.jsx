2a159413fd14a7e5621400ec220f557406d8dc19
elias <elias>
2025-03-09 02:01:46 +0100

    Fix

--- src/common/components/PageLayout.jsx
+++ src/common/components/PageLayout.jsx
@@ -58,7 +58,7 @@ const PageTitle = ({ breadcrumbs }) => {
 
   if (desktop) {
     return (
-      <Typography variant="h6" noWrap>{title ?? t(breadcrumbs[0] ?? breadcrumb)}</Typography>
+      <Typography variant="h6" noWrap>{t(breadcrumbs[0] ?? breadcrumb)}</Typography>
     );
   }
   return (
